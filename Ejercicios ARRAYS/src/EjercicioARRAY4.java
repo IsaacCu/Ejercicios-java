14c9a1b8613efa807738ddd7a688003a2f080435
IsaacCu <isaaccujilema2006@gmail.com>
2026-03-03 07:07:34 -0500

    fin

--- Ejercicios ARRAYS/src/EjercicioARRAY4.java
+++ Ejercicios ARRAYS/src/EjercicioARRAY4.java
@@ -13,24 +13,25 @@ public class EjercicioARRAY4 {
     public static void main(String[] args){
         Scanner sc =new Scanner(System.in);
         int[] codigosPagosAlDia = {
-            1021, 1050, 1103, 1155, 1201,
+            1021, 1050, 1103, 1155, 1221,
             1258, 1304, 1350, 1402, 1456,
             1500, 1555, 1601, 1650, 1703,
             1750, 1808, 1855, 1900, 1954,
             2001, 2050, 2100, 2155, 2203
         };
+
         System.out.println("Ingrese el numero de cedula: ");
         int busqueda = sc.nextInt();
         
+        boolean encontrado= false;
         int inicio=0;
         int fin= codigosPagosAlDia.length-1;
-        int ubicacionEncontrada=-1;
 
-        while (inicio<fin) {
+        while (inicio<=fin) {
             int medio=inicio+(fin-inicio)/2;
 
             if (codigosPagosAlDia[medio]==busqueda) {
-                ubicacionEncontrada=medio;
+                encontrado= true;
                 break;
             }
 
@@ -42,18 +43,15 @@ public class EjercicioARRAY4 {
             }
         }
 
-        if (ubicacionEncontrada != -1) {
+        if (encontrado) {
+
             System.out.println("El cliente esta al dia con su pago.");
-            System.out.println("ubicacion:"+ubicacionEncontrada);
         }
         else{
             System.out.println("El cliente no se encuentra al dia con su pago.");
         }
 
         sc.close();
-
-
-        
     }
 
 
