6255ac46dd7428d2c42ac8ec8cd0ffb4a01561d2
IsaacCu <isaaccujilema2006@gmail.com>
2026-03-03 06:54:14 -0500

    actualizacion

--- Ejercicios ARRAYS/src/EjercicioARRAY4.java
+++ Ejercicios ARRAYS/src/EjercicioARRAY4.java
@@ -8,17 +8,20 @@
 // Algoritmo a usar: Búsqueda Binaria. Es eficiente para buscar códigos numéricos en una lista
 //  que ya está organizada.
 
+import java.util.Scanner;
 public class EjercicioARRAY4 {
     public static void main(String[] args){
+        Scanner sc =new Scanner(System.in);
         int[] codigosPagosAlDia = {
-            1021, 1050, 1103, 1155, 1200,
+            1021, 1050, 1103, 1155, 1201,
             1258, 1304, 1350, 1402, 1456,
             1500, 1555, 1601, 1650, 1703,
             1750, 1808, 1855, 1900, 1954,
             2001, 2050, 2100, 2155, 2203
         };
-
-        int busqueda=1601;
+        System.out.println("Ingrese el numero de cedula: ");
+        int busqueda = sc.nextInt();
+        
         int inicio=0;
         int fin= codigosPagosAlDia.length-1;
         int ubicacionEncontrada=-1;
@@ -46,6 +49,8 @@ public class EjercicioARRAY4 {
         else{
             System.out.println("El cliente no se encuentra al dia con su pago.");
         }
+
+        sc.close();
 
 
         
