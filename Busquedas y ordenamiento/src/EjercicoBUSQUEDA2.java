ca42ae285735cfdcc17ded63d3d9186777e76fb6
IsaacCu <isaaccujilema2006@gmail.com>
2026-03-10 07:44:51 -0500

    punto4

--- Busquedas y ordenamiento/src/EjercicoBUSQUEDA2.java
+++ Busquedas y ordenamiento/src/EjercicoBUSQUEDA2.java
@@ -17,25 +17,44 @@ import java.util.Scanner;
 public class EjercicoBUSQUEDA2 {
     public static void main(String[] args){
         Scanner sc =new Scanner(System.in);
+
         System.out.println("Ingrese la cantidad de libros: ");
         int libros= sc.nextInt();
         int[] codigos= new int[libros];
 
-        for(int i=0; i<codigos.length; i++){
-            System.out.println("Ingrese el codigo del libro "+(i+1)+": ");
+        for(int x=0; x<codigos.length; x++){
+            System.out.println("Ingrese el codigo del libro "+(x+1)+": ");
             int codigo=sc.nextInt();
-            codigos[i]=codigo;
+            codigos[0]=codigo;
+
+            // Algoritmo de Inserción
+            int n = codigos.length;
+            for (int i = 1; i < n; i++) {
+                int clave = codigos[i];
+                int j = i - 1;
+
+                while (j >= 0 && codigos[j] > clave) {
+                    codigos[j + 1] = codigos[j];
+                    j = j - 1;  
+                }
+                codigos[j + 1] = clave; 
+            }
+            //imprimir proceso
+            
+            System.out.println("Organizando lista....");
+            imprimirArreglo(codigos);
         }
-
         
-
-
-
-
+        //Imprimir lista final
+        System.out.println("Lista organizada.");
+        imprimirArreglo(codigos);
 
         sc.close();
-
-
-
+    }
+    public static void imprimirArreglo(int[] arr) {
+        for (int d : arr) {
+            System.out.print("[" + d + "] ");
+        }
+        System.out.println();
     }
 }
---
+++ Busquedas y ordenamiento/src/EjercicoBUSQUEDA5.java
@@ -0,0 +1,26 @@
+// 5. El Inspector de Eficiencia (Duelo de Métodos)
+// Algoritmos obligatorios: Selección vs Inserción
+
+// Contexto: Un analista de sistemas quiere saber cuál algoritmo es más "pesado" 
+// en términos de operaciones para un caso específico.
+// Detalle del ejercicio: El usuario debe ingresar 6 números enteros desordenados. 
+// El programa aplicará AMBOS métodos al mismo conjunto de datos (puedes usar una 
+// copia del arreglo original).
+// Lógica de Conteo:
+// En Selección: Cuenta cada vez que ocurre un swap (intercambio físico de posiciones).
+// En Inserción: Cuenta cada vez que un elemento se "mueve" hacia la izquierda dentro 
+// del bucle interno.
+// Resultado esperado: El programa debe imprimir cuántos intercambios hizo Selección y 
+// cuántos movimientos hizo Inserción. El estudiante debe concluir cuál fue más eficiente 
+// para esos datos ingresados por el teclado.
+
+import java.util.Scanner;
+
+public class EjercicoBUSQUEDA5 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+
+        sc.close();
+    }
+}
