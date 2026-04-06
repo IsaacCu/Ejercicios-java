c6f4e5f4915d1ea5c822887b89413d3bc8459429
IsaacCu <isaaccujilema2006@gmail.com>
2026-04-06 11:02:48 -0500

    punto4

--- Corte 2/Nodos Simples/Ejercicio 4/src/App.java
+++ Corte 2/Nodos Simples/Ejercicio 4/src/App.java
@@ -1,5 +1,55 @@
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+// 4. Monitoreo Industrial (Sensores en Tiempo Real)
+// Una planta química registra lecturas de sus tanques cada hora en una lista enlazada para auditoría.
+
+// La Clase Lectura (Nodo): Debe contener idSensor (int), temperatura (double), presion (double) 
+// y hora (String).
+// El Problema: Las lecturas se van agregando al inicio para que la más reciente sea siempre la 
+// primera que vea el supervisor.
+// Reto: Implementa un método que busque y muestre la lectura con la temperatura más alta registrada 
+// en el historial.
+    public static void main(String[] args){
+
+        int opcion;
+        Historial historial = new Historial();
+        Scanner sc = new Scanner(System.in);
+        do {
+            System.out.println("========================================");
+            System.out.println("========== Menu de Opciones ============");
+            System.out.println("========================================");
+            System.out.println("1- Agregar lectura.");
+            System.out.println("2- Buscar la lectura mas alta.");
+            System.out.println("3- Salir.");
+            System.out.println("========================================");
+            System.out.println("Opcion: ");
+            opcion = sc.nextInt();
+            switch (opcion) {
+                case 1:
+                    int idSensor;
+                    double temperatura;
+                    double presion;
+                    String hora;
+                    System.out.println("Ingreseel Id del sensor: ");
+                    idSensor = sc.nextInt();
+                    System.out.println("Ingrese la temperatura:");
+                    temperatura = sc.nextDouble();
+                    System.out.println("Ingrese la presion: ");
+                    presion = sc.nextDouble();
+                    System.out.println("Ingrese la hora: ");
+                    hora = sc.next();
+                    Lectura nuevalectura = new Lectura(idSensor, temperatura, presion, hora);
+                    historial.agregar_lectura(nuevalectura);
+                    break;
+                case 2:
+                    historial.buscarMayorLectura();
+                    break;
+                default:
+                    System.out.println("Opcion invalida, elija otra opcion.");
+                    break;
+            }
+        } while (opcion != 3);
+        sc.close();
     }
 }
---
+++ Corte 2/Nodos Simples/Ejercicio 4/src/Historial.java
@@ -0,0 +1,41 @@
+public class Historial {
+    Lectura cabeza;
+    public Historial(){
+        this.cabeza = null;
+    }
+    public void agregar_lectura(Lectura nuevaLectura){
+        if (cabeza == null) {
+            this.cabeza = nuevaLectura;
+        }
+        else{
+            nuevaLectura.siguiente = cabeza;
+            cabeza = nuevaLectura;
+        }
+    }
+    public void buscarMayorLectura(){
+        if (cabeza == null) {
+            System.out.println("El historial se encuentra vacio.");   
+        }
+        else{
+            Lectura actual = cabeza;
+            Double bandera = cabeza.temperatura;
+            while (actual != null) {
+                if (actual.temperatura>bandera) {
+                    bandera = actual.temperatura;
+                }
+                actual = actual.siguiente;
+
+
+            }
+            actual = cabeza;
+            while (actual != null) {
+                if (actual.temperatura == bandera) {
+                    System.out.println("La lectura mas alta en el historial fue:");
+                    System.out.println("ID sensor:"+actual.idSensor+", Temperatura:"+actual.temperatura+", Presion:"+actual.presion+", Hora:"+actual.hora);
+                    return;
+                }
+                actual= actual.siguiente;
+            }        
+        }
+    }
+}
